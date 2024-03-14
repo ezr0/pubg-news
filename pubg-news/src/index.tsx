754cddcd9631cce6e38c56d86038a40e88c82470
Craig Nelson <craignelson88@googlemail.com>
2024-03-14 15:19:37 +0000

    Switch to typescript.

--- pubg-news/src/index.tsx
+++ pubg-news/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import '@fontsource/inter';
 
-import Login from './pages/Login';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import Login from './routes/Login';
+import Home from './routes/Home';
+import NotFound from './routes/NotFound';
+import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
     {
@@ -27,7 +28,9 @@ const router = createBrowserRouter([
     }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
 root.render(
     <React.StrictMode>
         <CssVarsProvider defaultMode='dark'>
--- pubg-news/src/routes/Home.tsx
+++ pubg-news/src/routes/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from '../components/header/Header';
+import React from 'react'
+import Header from '../components/header/Header'
 
 const Home = () => {
     return (
